e8da445ca5e685e3f0bfef1ac322e1d1add23f3c
YaoPeixuan <53012396+YaoPeixuan@users.noreply.github.com>
2020-03-11 21:39:47 +0800

    Add files via upload

---
+++ Version modularity/Struct.h
@@ -0,0 +1,18 @@
+//This file is about decrelation
+
+// tree data structure
+
+struct qnode {
+  int level;
+  double xy[2];
+  struct qnode *child[4];
+};
+typedef struct qnode Node;
+
+// function definitions
+
+Node *makeNode( double x, double y, int level );
+void makeChildren( Node *parent );
+void writeTree( Node *head );
+void writeNode( FILE *fp, Node *node );
+void printOut( FILE *fp, Node *node );
